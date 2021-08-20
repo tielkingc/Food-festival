c9ed7ef0aa79fcd749d9874720db734a2a43763b
Christian Tielking <tielking5@gmail.com>
2021-08-20 12:37:38 -0400

    code snapshot

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 require('bootstrap');
-const img1 = require("../../assets/img/food-table.jpg");
-const img2 = require("../../assets/img/grill.jpg");
+const img1 = require("../img/food-table.jpg");
+const img2 = require("../img/grill.jpg");
 
 $(document).ready(function() {
   // First image is hard coded in index.html
@@ -19,8 +19,8 @@ $(document).ready(function() {
       btnText: "Purchase Tickets",
       btnUrl: "tickets.html"
     },
-  ]
-
+  ];
+  
   carouselSlides.forEach((slide, i) => {
     $('.carousel-inner').append(`
   <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
